ac347cd3ee018b1ff5d518a3d60c096913513f70
Alexander A. Klimov <a@kli.mov>
2023-08-26 08:53:25 +0200

    Log libs

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/Al2Klimov/opensmtpd-filter-qdgrey
+
+go 1.20
+
+require (
+	github.com/Al2Klimov/go-gen-source-repos v0.2.0
+	github.com/sirupsen/logrus v1.9.3
+)
+
+require golang.org/x/sys v0.11.0 // indirect
---
+++ main.go
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package main
+
+import (
+	"github.com/Al2Klimov/go-gen-source-repos"
+	log "github.com/sirupsen/logrus"
+	"os"
+)
+
+func main() {
+	log.SetOutput(os.Stderr)
+	log.SetLevel(log.TraceLevel)
+
+	log.WithFields(log.Fields{"projects": source_repos.GetLinks()}).Info(
+		"For the terms of use, the source code and the authors see the projects this program is assembled from",
+	)
+}
