a12ac325e642a13b89bbb74bc20435cefdfea8e5
Alexander A. Klimov <a@kli.mov>
2023-08-26 09:07:43 +0200

    Exit on EOF

--- main.go
+++ main.go
@@ -3,8 +3,10 @@
 package main
 
 import (
+	"bufio"
 	"github.com/Al2Klimov/go-gen-source-repos"
 	log "github.com/sirupsen/logrus"
+	"io"
 	"os"
 )
 
@@ -16,4 +18,17 @@ func main() {
 	log.WithFields(log.Fields{"projects": source_repos.GetLinks()}).Info(
 		"For the terms of use, the source code and the authors see the projects this program is assembled from",
 	)
+
+	for in := bufio.NewReader(os.Stdin); ; {
+		switch line, err := in.ReadString('\n'); err {
+		case nil:
+			// TODO
+			_ = line
+		case io.EOF:
+			log.Info("End of stdin, terminating")
+			return
+		default:
+			log.WithError(err).Fatal("Couldn't read stdin")
+		}
+	}
 }
